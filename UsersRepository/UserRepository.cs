88621a135ea37764b88047b6f7d1c2fc25f1b24b
ARTHUR SEDRAKYAN <sedrakyan1998@gmail.com>
2018-07-21 16:43:21 +0500

    2

--- UsersRepository/UserRepository.cs
+++ UsersRepository/UserRepository.cs
@@ -23,7 +23,7 @@ namespace UsersRepository
         {
             var cnnStringBuilder = new SqlConnectionStringBuilder
             {
-                DataSource = @"MAXIM-PC\SQLSERVER",
+                DataSource = ".",
                 InitialCatalog = "UsersDB",
                 IntegratedSecurity = true
             };
