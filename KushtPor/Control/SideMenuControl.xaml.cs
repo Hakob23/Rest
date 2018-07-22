85ce1f9b293e8473a9725ac8016572b2c5e5ca15
maxim <maxim.karapetyan@gmail.com>
2018-07-22 13:28:56 +0400

    need to add pages tables

--- KushtPor/Control/SideMenuControl.xaml.cs
+++ KushtPor/Control/SideMenuControl.xaml.cs
@@ -20,9 +20,48 @@ namespace KushtPor
     /// </summary>
     public partial class SideMenuControl : UserControl
     {
+        public delegate void MyEventHandler(string name);
+
+        public event MyEventHandler OnNavigateParentReady;
+
         public SideMenuControl()
         {
             InitializeComponent();
+        }
+
+        private void AlcoholDrinks_Click(object sender, RoutedEventArgs e)
+        {
+            OnNavigateParentReady("AlcoholDrinks");
+        }
+
+        private void Burgers_Click(object sender, RoutedEventArgs e)
+        {
+            OnNavigateParentReady("Burgers");
+        }
+
+        private void Drinks_Click(object sender, RoutedEventArgs e)
+        {
+            OnNavigateParentReady("Drinks");
+        }
+
+        private void HotMeals_Click(object sender, RoutedEventArgs e)
+        {
+            OnNavigateParentReady("HotMeals");
+        }
+
+        private void Pizzas_Click(object sender, RoutedEventArgs e)
+        {
+            OnNavigateParentReady("Pizzas");
+        }
+
+        private void Salads_Click(object sender, RoutedEventArgs e)
+        {
+            OnNavigateParentReady("Salads");
+        }
+
+        private void Soups_Click(object sender, RoutedEventArgs e)
+        {
+            OnNavigateParentReady("Soups");
         }
     }
 }
