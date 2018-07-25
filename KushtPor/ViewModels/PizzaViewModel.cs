fc4b4c2056df8fc151dc38974d9afa26214441ad
maxim <maxim.karapetyan@gmail.com>
2018-07-26 01:46:56 +0400

    pizza page modifyed and added buttons , corrected viewmodel

--- KushtPor/ViewModels/PizzaViewModel.cs
+++ KushtPor/ViewModels/PizzaViewModel.cs
@@ -1,16 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using KushtPor.Commands;
+using System;
+using System.Collections.ObjectModel;
 using System.Net.Http;
 using System.Net.Http.Headers;
 
 namespace KushtPor.ViewModels
 {
-    class PizzaViewModel
+    /// <summary>
+    /// Pizzas view model
+    /// </summary>
+    class PizzaViewModel:ViewModel
     {
-        public List<Pizza> Pizzas;
+        // List from where gets data
+        public ObservableCollection<Pizza> Pizzas { get; set; }
+        
+        // access Token
         public string accessToken;
+
+        // username
         public string username;
+
+        // http client
         private HttpClient client;
+
+        /// <summary>
+        /// Pizza Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Pizza Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Pizza Price
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Pizzas Content
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// Pizzas Diametr
+        /// </summary>
+        public int Diametr { get; set; }
+
+        /// <summary>
+        /// Pizza Add Name
+        /// </summary>
+        public string AddName { get; set; }
+
+        /// <summary>
+        /// Pizza Add Price
+        /// </summary>
+        public double AddPrice { get; set; }
+
+        /// <summary>
+        /// Pizzas Add Content
+        /// </summary>
+        public string AddContent { get; set; }
+
+        /// <summary>
+        /// Pizzas Add Diametr
+        /// </summary>
+        public int AddDiametr { get; set; }
+
+
+
+        /// <summary>
+        /// Delete button command
+        /// </summary>
+        public RelayCommand Delete { get; set; }
+
+        /// <summary>
+        /// Add button command
+        /// </summary>
+        public RelayCommand Add { get; set; }
 
         /// <summary>
         /// Pizza view model constructor
@@ -26,7 +94,7 @@ namespace KushtPor.ViewModels
             this.username = username;
             
             // Pizzas
-            Pizzas = new List<Pizza>();
+            //Pizzas = new List<Pizza>();
 
             // create http client instance
             client = new HttpClient();
@@ -45,9 +113,37 @@ namespace KushtPor.ViewModels
             if (response.IsSuccessStatusCode)
             {
                // init Piizas
-               Pizzas = response.Content.ReadAsAsync<List<Pizza>>().Result;
+               Pizzas = response.Content.ReadAsAsync<ObservableCollection<Pizza>>().Result;
             }
 
+            Delete = new RelayCommand(() => DeletePizza(), o => true);
+            Add = new RelayCommand(() => AddPizzaAsync(), o => true);
+        }
+
+        public async System.Threading.Tasks.Task AddPizzaAsync()
+        {
+            var pizza = new Pizza();
+            pizza.Name = AddName;
+            pizza.Price = AddPrice;
+            pizza.Content = AddContent;
+            pizza.Diametr = AddDiametr;
+            pizza.Restaurant = username;
+
+            var response = await client.PostAsJsonAsync($"api/pizzas", pizza);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Pizzas.Add(pizza);
+            }
+        }
+
+        public void DeletePizza()
+        {
+            var pizza = new Pizza();
+            pizza.Id = Id;
+            pizza.Price = Price;
+            pizza.Name = Name;
+            pizza.Diametr = Diametr;
         }
     }
 }
